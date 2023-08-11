6ed9fc03899b09a1755d0f5d7e78397480ba8705
Ashen Sandaruwan <59907222+ashen92@users.noreply.github.com>
2023-08-11 21:37:43 +0530

    Remove Base/Framework

--- Src/Web/App/public/index.php
+++ Src/Web/App/public/index.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__."/../vendor/autoload.php";
+require_once __DIR__ . "/../vendor/autoload.php";
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,7 +10,7 @@ use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 
 $request = Request::createFromGlobals();
-$routes = include __DIR__."/../src/app.php";
+$routes = include __DIR__ . "/../src/app.php";
 
 $context = new Routing\RequestContext();
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
@@ -18,7 +18,17 @@ $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 $controllerResolver = new ControllerResolver();
 $argumentResolver = new ArgumentResolver();
 
-$framework = new Base\Framework($matcher, $controllerResolver, $argumentResolver);
-$response = $framework->handle($request);
+try {
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+
+    $controller = $controllerResolver->getController($request);
+    $arguments = $argumentResolver->getArguments($request, $controller);
+
+    $response = call_user_func_array($controller, $arguments);
+} catch (Routing\Exception\ResourceNotFoundException $exception) {
+    $response = new Response('Not Found', 404);
+} catch (Exception $exception) {
+    $response = new Response('An error occurred', 500);
+}
 
 $response->send();
