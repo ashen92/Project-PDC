58ddd85b49badb51353f8aa287d8515f201ce5b7
Ashen Sandaruwan <59907222+ashen92@users.noreply.github.com>
2023-11-10 02:12:30 +0530

    Add required to input fields

--- Src/Web/App/public/js/requirement-add.js
+++ Src/Web/App/public/js/requirement-add.js
@@ -4,13 +4,23 @@
   var recurringRepeatElement = document.getElementById("field-recurring");
   var radioOneTimeElement = document.getElementById("type-one-time");
   var radioRecurringElement = document.getElementById("type-recurring");
+  var endBeforeDateElement = document.getElementById("end-before-one-time");
+  var radioRepeatElements = document.querySelectorAll("input[name='repeat']");
   radioOneTimeElement.addEventListener("change", function() {
+    radioRepeatElements.forEach((element) => {
+      element.removeAttribute("required");
+    });
+    endBeforeDateElement.setAttribute("required", "");
     oneTimeEndDateElement.classList.add("block");
     oneTimeEndDateElement.classList.remove("hidden");
     recurringRepeatElement.classList.add("hidden");
     recurringRepeatElement.classList.remove("block");
   });
   radioRecurringElement.addEventListener("change", function() {
+    radioRepeatElements.forEach((element) => {
+      element.setAttribute("required", "");
+    });
+    endBeforeDateElement.removeAttribute("required");
     oneTimeEndDateElement.classList.add("hidden");
     oneTimeEndDateElement.classList.remove("block");
     recurringRepeatElement.classList.add("block");
--- Src/Web/App/src/wwwroot/js/requirement-add.js
+++ Src/Web/App/src/wwwroot/js/requirement-add.js
@@ -5,7 +5,17 @@ const recurringRepeatElement = document.getElementById("field-recurring");
 const radioOneTimeElement = document.getElementById("type-one-time");
 const radioRecurringElement = document.getElementById("type-recurring");
 
+const endBeforeDateElement = document.getElementById("end-before-one-time");
+const radioRepeatElements = document.querySelectorAll("input[name='repeat']");
+
 radioOneTimeElement.addEventListener("change", function () {
+
+    radioRepeatElements.forEach(element => {
+        element.removeAttribute("required");
+    });
+
+    endBeforeDateElement.setAttribute("required", "");
+
     oneTimeEndDateElement.classList.add("block");
     oneTimeEndDateElement.classList.remove("hidden");
     recurringRepeatElement.classList.add("hidden");
@@ -13,6 +23,13 @@ radioOneTimeElement.addEventListener("change", function () {
 });
 
 radioRecurringElement.addEventListener("change", function () {
+
+    radioRepeatElements.forEach(element => {
+        element.setAttribute("required", "");
+    });
+
+    endBeforeDateElement.removeAttribute("required");
+
     oneTimeEndDateElement.classList.add("hidden");
     oneTimeEndDateElement.classList.remove("block");
     recurringRepeatElement.classList.add("block");
