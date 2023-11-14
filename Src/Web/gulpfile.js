28fa292751256ac18bb9bc1dfa5c5da4c212f406
Ashen Sandaruwan <59907222+ashen92@users.noreply.github.com>
2023-11-14 20:49:44 +0530

    Remove live sass compiler

--- Src/Web/gulpfile.js
+++ Src/Web/gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require("gulp");
 const esbuild = require("esbuild");
+const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("gulp-autoprefixer");
+const sourcemaps = require('gulp-sourcemaps');
 const cleanCSS = require("gulp-clean-css");
 const rename = require("gulp-rename");
 
@@ -34,5 +37,33 @@ gulp.task("watch-css", function () {
     gulp.watch("./App/src/wwwroot/css/*.css", gulp.series("build-css"));
 });
 
+// SCSS tasks
+
+gulp.task("build-scss", function () {
+    return gulp.src("./App/src/wwwroot/scss/main.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+        .pipe(autoprefixer({
+            overrideBrowserslist: [
+                ">0.15%",
+                "defaults",
+                "not dead"
+            ],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write("./"))
+        .pipe(gulp.dest("./App/public/css"));
+});
+
+gulp.task("watch-scss", function () {
+    gulp.watch("./App/src/wwwroot/scss/*.css", gulp.series("build-scss"));
+});
+
 // Default task
-gulp.task("default", gulp.parallel(gulp.series("build-js", "watch-js"), gulp.series("build-css", "watch-css")));
+gulp.task(
+    "default",
+    gulp.parallel(
+        gulp.series("build-js", "watch-js"),
+        gulp.series("build-css", "watch-css"),
+        gulp.series("build-scss", "watch-scss")
+    ));
