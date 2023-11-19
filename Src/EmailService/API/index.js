4ab439bef0d735522ffa6c0b3fc7efbc35b28f42
Ashen Sandaruwan <59907222+ashen92@users.noreply.github.com>
2023-11-19 11:19:05 +0530

    Update api to use async and await

--- Src/EmailService/API/index.js
+++ Src/EmailService/API/index.js
@@ -16,116 +16,78 @@ async function connectClient() {
 
 connectClient();
 
-// Create a HTTP server
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     res.setHeader("Content-Security-Policy", "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self';");
     res.setHeader("X-Frame-Options", "DENY");
     res.setHeader("X-Content-Type-Options", "nosniff");
     res.setHeader("X-XSS-Protection", "1; mode=block");
 
-    // #region Rate limit
+    try {
+        await redisClient.select(1);
+        const ip = req.socket.remoteAddress;
+        let record = await redisClient.get(ip);
 
-    redisClient.select(1)
-        .then(() => {
-            const ip = req.socket.remoteAddress;
+        if (record) {
+            record = parseInt(record);
+            if (record >= 5000) {
+                res.writeHead(429);
+                res.end("Too Many Requests");
+                return;
+            }
+        }
 
-            redisClient.get(ip)
-                .then(record => {
-                    // If the IP address has made too many requests, return a 429 response
-                    if (record) {
-                        record = parseInt(record);
-                        if (record >= 5000) {
-                            res.writeHead(429);
-                            res.end("Too Many Requests");
-                            return;
-                        }
-                    }
+        await redisClient.set(ip, (record || 0) + 1, "EX", 3600);
 
-                    // Otherwise, increment the count for the IP address
-                    redisClient.set(ip, (record || 0) + 1, "EX", 3600)
-                        .then(() => {
-                            // #region Handle API requests
+        await redisClient.select(0);
+        const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+        const path = parsedUrl.pathname;
+        const trimmedPath = path.replace(/^\/+|\/+$/g, "");
 
-                            redisClient.select(0)
-                                .then(() => {
-                                    // Parse the request URL
-                                    const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
-                                    const path = parsedUrl.pathname;
-                                    const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+        if (trimmedPath === "api/emails" && req.method === "POST") {
+            const apiKey = req.headers["x-api-key"];
+            if (apiKey !== process.env.PDC_WEBSITE_API_KEY) {
+                res.writeHead(403);
+                res.end("Invalid API key");
+                return;
+            }
 
-                                    // Check for POST request to specific path
-                                    if (trimmedPath === "api/emails" && req.method === "POST") {
-                                        // Check the API key
-                                        const apiKey = req.headers["x-api-key"]; // Assuming API key is sent in 'x-api-key' header
-                                        if (apiKey !== process.env.PDC_WEBSITE_API_KEY) {
-                                            res.writeHead(403);
-                                            res.end("Invalid API key");
-                                            return;
-                                        }
+            const decoder = new StringDecoder("utf-8");
+            let buffer = "";
 
-                                        // Handle POST data
-                                        const decoder = new StringDecoder("utf-8");
-                                        let buffer = "";
+            req.on("data", (data) => {
+                buffer += decoder.write(data);
+            });
 
-                                        req.on("data", (data) => {
-                                            buffer += decoder.write(data);
-                                        });
+            req.on("end", async () => {
+                buffer += decoder.end();
 
-                                        req.on("end", () => {
-                                            buffer += decoder.end();
-
-                                            // Parse the email data
-                                            let emailData;
-                                            try {
-                                                emailData = JSON.parse(buffer);
-                                            } catch (e) {
-                                                res.writeHead(400);
-                                                res.end("Invalid JSON");
-                                                return;
-                                            }
-
-                                            // Queue the email data in Redis
-                                            redisClient.lPush("emailQueue", JSON.stringify(emailData))
-                                                .then(reply => {
-                                                    res.writeHead(201, { "Content-Type": "application/json" });
-                                                    res.end(JSON.stringify({ message: "Email queued successfully" }));
-                                                })
-                                                .catch(err => {
-                                                    res.writeHead(500);
-                                                    res.end("Error queuing email");
-                                                });
-                                        });
-                                    } else {
-                                        res.writeHead(404);
-                                        res.end("Not Found");
-                                    }
-                                })
-                                .catch(err => {
-                                    console.error("Error selecting Redis database", err);
-                                });
-
-                            // #endregion
-                        })
-                        .catch(err => {
-                            res.writeHead(500);
-                            res.end("Error updating rate limit record");
-                            return;
-                        });
-                })
-                .catch(err => {
-                    res.writeHead(500);
-                    res.end("Error reading rate limit record");
+                let emailData;
+                try {
+                    emailData = JSON.parse(buffer);
+                } catch (e) {
+                    res.writeHead(400);
+                    res.end("Invalid JSON");
                     return;
-                });
-        })
-        .catch(err => {
-            console.error("Error selecting Redis database", err);
-        });
+                }
 
-    // #endregion
+                try {
+                    await redisClient.lPush("emailQueue", JSON.stringify(emailData));
+                    res.writeHead(201, { "Content-Type": "application/json" });
+                    res.end(JSON.stringify({ message: "Email queued successfully" }));
+                } catch (err) {
+                    res.writeHead(500);
+                    res.end("Error queuing email");
+                }
+            });
+        } else {
+            res.writeHead(404);
+            res.end("Not Found");
+        }
+    } catch (err) {
+        console.error("Error selecting Redis database", err);
+    }
 });
 
-// Start the server
 const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
