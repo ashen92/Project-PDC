007bc7fca95024c1f7d995d871d48e0b5d1c8f72
Ashen Sandaruwan <59907222+ashen92@users.noreply.github.com>
2023-12-01 12:40:27 +0530

    Change event listeners

--- Src/Web/App/src/wwwroot/js/requirement-add.js
+++ Src/Web/App/src/wwwroot/js/requirement-add.js
@@ -1,39 +1,38 @@
-
 const oneTimeEndDateElement = document.getElementById("field-one-time");
 const recurringRepeatElement = document.getElementById("field-recurring");
-
-const radioOneTimeElement = document.getElementById("type-one-time");
-const radioRecurringElement = document.getElementById("type-recurring");
 
 const endBeforeDateElement = document.getElementById("end-before-one-time");
 const radioRepeatElements = document.querySelectorAll("input[name='repeat-interval']");
 
-radioOneTimeElement.addEventListener("change", function () {
+const typeDiv = document.getElementById("type");
 
-    radioRepeatElements.forEach(element => {
-        element.removeAttribute("required");
-    });
+typeDiv.addEventListener("change", function (event) {
+    if (event.target.type === "radio") {
+        if (event.target.value == "one-time") {
+            radioRepeatElements.forEach(element => {
+                element.removeAttribute("required");
+            });
 
-    endBeforeDateElement.setAttribute("required", "");
+            endBeforeDateElement.setAttribute("required", "");
 
-    oneTimeEndDateElement.classList.add("block");
-    oneTimeEndDateElement.classList.remove("hidden");
-    recurringRepeatElement.classList.add("hidden");
-    recurringRepeatElement.classList.remove("block");
-});
+            oneTimeEndDateElement.classList.add("block");
+            oneTimeEndDateElement.classList.remove("hidden");
+            recurringRepeatElement.classList.add("hidden");
+            recurringRepeatElement.classList.remove("block");
+        }
+        else if (event.target.value == "recurring") {
+            radioRepeatElements.forEach(element => {
+                element.setAttribute("required", "");
+            });
 
-radioRecurringElement.addEventListener("change", function () {
+            endBeforeDateElement.removeAttribute("required");
 
-    radioRepeatElements.forEach(element => {
-        element.setAttribute("required", "");
-    });
-
-    endBeforeDateElement.removeAttribute("required");
-
-    oneTimeEndDateElement.classList.add("hidden");
-    oneTimeEndDateElement.classList.remove("block");
-    recurringRepeatElement.classList.add("block");
-    recurringRepeatElement.classList.remove("hidden");
+            oneTimeEndDateElement.classList.add("hidden");
+            oneTimeEndDateElement.classList.remove("block");
+            recurringRepeatElement.classList.add("block");
+            recurringRepeatElement.classList.remove("hidden");
+        }
+    }
 });
 
 const fulfillMethodDiv = document.getElementById("fulfill-method");
@@ -42,8 +41,6 @@ const fulfillMethodFileOptions = document.getElementById("fulfill-method-file-op
 fulfillMethodDiv.addEventListener("change", function (event) {
     if (event.target.name === "fulfill-method") {
         if (event.target.checked) {
-            console.log("Selected method: " + event.target.value);
-
             if (event.target.value == "file") {
                 fulfillMethodFileOptions.classList.remove("hidden");
                 fulfillMethodFileOptions.classList.add("block");
