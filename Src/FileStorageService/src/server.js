daea3074f2f5e07d805d7ef8d201b5b02cc1228b
Ashen Sandaruwan <59907222+ashen92@users.noreply.github.com>
2023-12-05 13:25:18 +0530

    Change server port. Change API endpoint

--- Src/FileStorageService/src/server.js
+++ Src/FileStorageService/src/server.js
@@ -1,4 +1,5 @@
 import http from "http";
+// import "dotenv/config";
 import { parseIncomingFile } from "./fileParser.js";
 import { uploadFileToAzure } from "./azureUpload.js";
 
@@ -8,7 +9,7 @@ const server = http.createServer(async (req, res) => {
     res.setHeader("X-Content-Type-Options", "nosniff");
     res.setHeader("X-XSS-Protection", "1; mode=block");
 
-    if (req.method === "POST" && req.url === "/upload") {
+    if (req.method === "POST" && req.url === "/api/files") {
         try {
             const files = await parseIncomingFile(req);
             let fileProperties = await uploadFileToAzure(files);
@@ -32,5 +33,5 @@ const server = http.createServer(async (req, res) => {
     }
 });
 
-const port = 3000;
+const port = process.env.SERVER_PORT || 3000;
 server.listen(port, () => console.log(`Server running on port ${port}`));
