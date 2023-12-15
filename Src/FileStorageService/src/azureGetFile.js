322fbc64b646469e6bb401405a783af567200fc8
Ashen Sandaruwan <59907222+ashen92@users.noreply.github.com>
2023-12-15 22:23:32 +0530

    Add support for getting a file from the file storage API

---
+++ Src/FileStorageService/src/azureGetFile.js
@@ -0,0 +1,25 @@
+import { BlobServiceClient } from "@azure/storage-blob";
+import "dotenv/config";
+
+const AZURE_STORAGE_CONNECTION_STRING =
+    process.env.AZURE_STORAGE_CONNECTION_STRING;
+
+if (!AZURE_STORAGE_CONNECTION_STRING) {
+    throw Error("Azure Storage Connection string not found");
+}
+
+const blobServiceClient = BlobServiceClient.fromConnectionString(
+    AZURE_STORAGE_CONNECTION_STRING
+);
+
+export async function getFileFromAzure(filePath, writableStream) {
+    const [containerName, blobName] = filePath.split("-uuid-");
+
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    const blobClient = containerClient.getBlobClient(blobName);
+
+    const downloadResponse = await blobClient.download();
+
+    downloadResponse.readableStreamBody.pipe(writableStream);
+    console.log(`Download of ${blobName} succeeded`);
+}
--- Src/FileStorageService/src/server.js
+++ Src/FileStorageService/src/server.js
@@ -2,6 +2,7 @@ import http from "http";
 // import "dotenv/config";
 import { parseIncomingFile } from "./fileParser.js";
 import { uploadFileToAzure } from "./azureUpload.js";
+import { getFileFromAzure } from "./azureGetFile.js";
 
 const server = http.createServer(async (req, res) => {
     res.setHeader("Content-Security-Policy", "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self';");
@@ -26,6 +27,17 @@ const server = http.createServer(async (req, res) => {
             console.error("Error parsing file:", error);
             res.writeHead(500);
             res.end("Internal Server Error");
+        }
+    } else if (req.method === "GET" && req.url.startsWith("/api/files/")) {
+        const filePath = req.url.slice("/api/files/".length);
+        const [containerName] = filePath.split("-uuid-");
+        try {
+            res.writeHead(200, { "Content-Type": containerName });
+            await getFileFromAzure(filePath, res);
+        } catch (error) {
+            console.error("Error getting file:", error);
+            res.writeHead(500);
+            res.end();
         }
     } else {
         res.writeHead(404);
