f7d65ab2ef50888ca9d942e35e79da9393fd366d
Ashen Sandaruwan <59907222+ashen92@users.noreply.github.com>
2024-01-05 01:18:15 +0530

    Refactor route loading logic and remove APCu extension

--- Src/Web/.docker/config/routes.php
+++ Src/Web/.docker/config/routes.php
@@ -8,17 +8,10 @@ use Symfony\Component\Routing\RouteCollection;
 
 $routes = new RouteCollection();
 
-$routesCache = apcu_fetch("routes_cache");
-
-if ($routesCache === false) {
-    $controllerDir = __DIR__ . "/Controllers";
-    $fileLocator = new FileLocator();
-    $routeAttributeLoader = new AttributeClassLoader();
-    $attributeDirectoryLoader = new AttributeDirectoryLoader($fileLocator, $routeAttributeLoader);
-    $routes->addCollection($attributeDirectoryLoader->load($controllerDir));
-    apcu_store("routes_cache", serialize($routes));
-} else {
-    $routes = unserialize($routesCache);
-}
+$controllerDir = __DIR__ . "/Controllers";
+$fileLocator = new FileLocator();
+$routeAttributeLoader = new AttributeClassLoader();
+$attributeDirectoryLoader = new AttributeDirectoryLoader($fileLocator, $routeAttributeLoader);
+$routes->addCollection($attributeDirectoryLoader->load($controllerDir));
 
 return $routes;
