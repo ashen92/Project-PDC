7cb7f98ccb74d41eddeb7b739221d85d99538b1b
Ashen Sandaruwan <59907222+ashen92@users.noreply.github.com>
2024-02-01 23:19:55 +0530

    Add table component to make rows clickable

--- Src/Web/App/src/wwwroot/js/components/main.js
+++ Src/Web/App/src/wwwroot/js/components/main.js
@@ -1,5 +1,7 @@
 import { filePicker } from "./filePicker.js";
 import { textEditor } from "./textEditor.js";
+import * as table from "./table.js";
 
 window.filePicker = filePicker;
 window.textEditor = textEditor;
+window.table = table;
---
+++ Src/Web/App/src/wwwroot/js/components/table.js
@@ -0,0 +1,31 @@
+import { $, $all } from "../core/dom.js";
+import { on } from "../core/events.js";
+
+function makeRowsClickable(rowContainerElementSelector) {
+    if (rowContainerElementSelector[0] === ".") {
+        const rowContainers = $all(rowContainerElementSelector);
+
+        for (let i = 0; i < rowContainers.length; i++) {
+            on(rowContainers[i], "click", function (event) {
+                const target = event.target;
+                const row = target.closest("tr");
+
+                if (row && !target.matches("input[type='checkbox']") && !target.closest("td").querySelector("input[type='checkbox']")) {
+                    window.location.href = row.dataset.href;
+                }
+            });
+        }
+        return;
+    }
+
+    on($(rowContainerElementSelector), "click", function (event) {
+        const target = event.target;
+        const row = target.closest("tr");
+
+        if (row && !target.matches("input[type='checkbox']") && !target.closest("td").querySelector("input[type='checkbox']")) {
+            window.location.href = row.dataset.href;
+        }
+    });
+}
+
+export { makeRowsClickable };
