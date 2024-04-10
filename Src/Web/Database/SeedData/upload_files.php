1372f93c4bf0c0d915ca2977e88c62df47a9da29
Ashen Sandaruwan <59907222+ashen92@users.noreply.github.com>
2024-04-10 18:45:38 +0530

    Add renamed image files to support drag and drop into the test container

---
+++ Src/Web/Database/SeedData/upload_files.php
@@ -0,0 +1,13 @@
+<?php
+
+$logoFileNames = [
+    'microsoft.jpeg' => 'image-jpeg-uuid-49df2eb0-9b7b-11ee-a730-9109ff162764',
+    'google.jpeg' => 'image-jpeg-uuid-49dbd350-9b7b-11ee-a730-9109ff162764',
+    'amazon.jpeg' => 'image-jpeg-uuid-49d850e0-9b7b-11ee-a730-9109ff162764',
+    'meta.jpeg' => 'image-jpeg-uuid-49dda810-9b7b-11ee-a730-9109ff162764',
+    'apple.jpeg' => 'image-jpeg-uuid-49da25a0-9b7b-11ee-a730-9109ff162764',
+    'wiley.jpeg' => 'image-jpeg-uuid-a26cf3f0-a265-11ee-96e3-d1f5316d3366',
+    'london_stock_exchange_group.jpeg' => 'image-jpeg-uuid-475b0860-a26c-11ee-96e3-d1f5316d3366',
+    'dialog_axiata.jpeg' => 'image-jpeg-uuid-ebffa2a0-a270-11ee-96e3-d1f5316d3366',
+    'lockheed_martin.jpeg' => 'image-jpeg-uuid-c3d893b0-a278-11ee-96e3-d1f5316d3366'
+];
