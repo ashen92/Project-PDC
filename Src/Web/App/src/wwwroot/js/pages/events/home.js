cbdbd5912c5bd88a71df7a889f06c5cbd490c55e
Bavindu <kmbbdh@gmail.com>
2024-04-21 00:10:02 +0530

    added daygrid, list, timegrid to the calendar

--- Src/Web/App/src/wwwroot/js/pages/events/home.js
+++ Src/Web/App/src/wwwroot/js/pages/events/home.js
@@ -1,11 +1,24 @@
 import { Calendar } from "@fullcalendar/core";
+import interactionPlugin from "@fullcalendar/interaction";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
 import { $ } from "../../core/dom";
 
-const calendar = new Calendar($("#calendar-view"), {
-    plugins: [dayGridPlugin],
-    initialView: "dayGridMonth"
-});
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
 
+
+const calendar = new Calendar($("#calendar-view"), {
+    plugins: [dayGridPlugin,timeGridPlugin, listPlugin,interactionPlugin, list],
+    initialView: "dayGridMonth",
+    selectable: true,
+    //events: '/events.php'
+    headerToolbar: {
+        left: "prev,next today",
+        center: "title",
+        right: "dayGridMonth,timeGridWeek,listWeek"
+    }
+});
 
 calendar.render();
