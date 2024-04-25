cc40950bf54ee8466100cf4e589b710a6114dd33
Ashen Sandaruwan <59907222+ashen92@users.noreply.github.com>
2024-04-25 09:39:59 +0530

    Remove user selection options

--- Src/Web/App/src/wwwroot/js/pages/internship-program/requirements/create.js
+++ Src/Web/App/src/wwwroot/js/pages/internship-program/requirements/create.js
@@ -1,36 +1,6 @@
-import { $, $all, createElement } from "../../../core/dom.js";
-import { on, trigger } from "../../../core/events.js";
+import { $, $all } from "../../../core/dom.js";
+import { on } from "../../../core/events.js";
 import { createTextEditors } from "../../../components/textEditor.js";
-
-on(document, "DOMContentLoaded", function () {
-    const selectedUsers = localStorage.getItem("selectUsers");
-    if (selectedUsers) {
-        userGroupRadio.forEach(function (element) {
-            if (element.value === "select-users") {
-                element.checked = true;
-                trigger(element, "change");
-            }
-        });
-    }
-    localStorage.removeItem("selectUsers");
-});
-
-const userGroupRadio = $all("#user-group-radio input[name='user-group']");
-const selectUsersField = $("#select-users-field");
-
-userGroupRadio.forEach(function (element) {
-    on(element, "change", function () {
-        if (this.value === "select-users") {
-            selectUsersField.classList.remove("hidden");
-        } else {
-            selectUsersField.classList.add("hidden");
-        }
-    });
-});
-
-on($("#select-users-btn"), "click", function () {
-    localStorage.setItem("selectUsers", "true");
-});
 
 createTextEditors();
 
@@ -72,29 +42,4 @@ on(fulfillMethodDiv, "change", function (event) {
             }
         }
     }
-});
-
-window.onbeforeunload = function () {
-    localStorage.removeItem("selectedUserIds");
-};
-
-let form = $("#create-requirement-form");
-
-on(form, "submit", function (event) {
-    userGroupRadio.forEach(function (element) {
-        if (element.checked && element.value === "select-users") {
-            const selectedUserIds = JSON.parse(localStorage.getItem("selectedUserIds")) || [];
-            if (selectedUserIds.length === 0) {
-                event.preventDefault();
-                alert("Please select users\nTODO: Show a better error message.");
-            }
-
-            let input = createElement("input", {
-                type: "hidden",
-                name: "user-ids",
-                value: JSON.stringify(selectedUserIds)
-            });
-            form.appendChild(input);
-        }
-    });
 });
