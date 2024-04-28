af57560d1d69385cb6ed4050e06783bf448b9217
Bavindu <kmbbdh@gmail.com>
2024-04-29 00:45:40 +0530

    removed list view from the event home

--- Src/Web/App/src/wwwroot/js/pages/events/home.js
+++ Src/Web/App/src/wwwroot/js/pages/events/home.js
@@ -1,12 +1,12 @@
 import { Calendar } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import listPlugin from "@fullcalendar/list";
+//import listPlugin from "@fullcalendar/list";
 import interactionPlugin from "@fullcalendar/interaction";
 import { $ } from "../../core/dom";
 
 const calendar = new Calendar($("#calendar-view"), {
-    plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
+    plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin], //, listPlugin
     initialView: "dayGridMonth",
     selectable: true,
     events: {
@@ -18,11 +18,12 @@ const calendar = new Calendar($("#calendar-view"), {
     },
     eventClick: function (info) {
         var eventObj = info.event;
-
+        var eventId= eventObj.id;
+    
         if (eventObj.url) {
             alert(
                 "Clicked " + eventObj.title + ".\n" +
-                "Will open " + eventObj.url + " in a new tab"
+                "Will open " + eventObj.url + " in a new tab",
             );
 
             window.open(eventObj.url);
@@ -40,7 +41,7 @@ const calendar = new Calendar($("#calendar-view"), {
     headerToolbar: {
         left: "prev,next today",
         center: "title",
-        right: "dayGridMonth,timeGridWeek,listWeek"
+        right: "dayGridMonth,timeGridWeek" //,listWeek
     }
 });
 
